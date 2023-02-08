38b5678e3a953b3d519adf666b52c12877e48fc3
Kevin Quach <kquach1337@gmail.com>
2023-02-08 15:25:19 -0500

    add home page tests, rename tests

--- src/test/java/org/kq/addressbook/AddressBookApplicationTests.java
+++ src/test/java/org/kq/addressbook/AddressBookApplicationTests.java
@@ -30,14 +30,14 @@ public class AddressBookApplicationTests {
         restTemplatePatch.getRestTemplate().setRequestFactory(new HttpComponentsClientHttpRequestFactory());
     }
     @Test
-    public void homeGet() throws Exception {
+    public void testIndexGet() throws Exception {
         String url = "http://localhost:" + port;
         assertThat(this.restTemplate.getForObject(url, String.class)).contains("addressBooks");
         assertThat(this.restTemplate.getForObject(url, String.class)).contains("buddyInfoes");
     }
 
     @Test
-    public void homePostAddressBook() throws Exception {
+    public void testPostAddressBook() throws Exception {
         String url = "http://localhost:" + port + "/addressBooks";
         HttpEntity<AddressBook> request = new HttpEntity<>(new AddressBook());
         AddressBook ab = restTemplate.postForObject(url, request, AddressBook.class);
@@ -46,7 +46,7 @@ public class AddressBookApplicationTests {
     }
 
     @Test
-    public void homePostBuddyInfo() throws Exception {
+    public void testPostBuddyInfo() throws Exception {
         String url = "http://localhost:" + port + "/buddyInfoes";
         String name = "Bob";
         String phoneNumber = "555-333-1111";
@@ -58,7 +58,7 @@ public class AddressBookApplicationTests {
         assertThat(this.restTemplate.getForObject(url, String.class)).contains("buddyInfoes/1");
     }
     @Test
-    public void homePatchAddBuddiesToAddressBook() throws Exception {
+    public void testPatchAddBuddiesToAddressBook() throws Exception {
         String url = "http://localhost:" + port;
 
         HttpEntity<AddressBook> requestAb = new HttpEntity<>(new AddressBook());
@@ -88,7 +88,7 @@ public class AddressBookApplicationTests {
         assertThat(this.restTemplate.getForObject(url + "/addressBooks/1/buddies", String.class)).contains(phoneNumber2);
     }
     @Test
-    public void homeDeleteBuddyFromAddressBook() throws Exception {
+    public void testDeleteBuddyFromAddressBook() throws Exception {
         String url = "http://localhost:" + port;
 
         HttpEntity<AddressBook> requestAb = new HttpEntity<>(new AddressBook());
@@ -118,5 +118,69 @@ public class AddressBookApplicationTests {
         assertThat(this.restTemplate.getForObject(url + "/addressBooks/1/buddies", String.class)).doesNotContain(phoneNumber);
         assertThat(this.restTemplate.getForObject(url + "/addressBooks/1/buddies", String.class)).contains(name2);
         assertThat(this.restTemplate.getForObject(url + "/addressBooks/1/buddies", String.class)).contains(phoneNumber2);
+    }
+
+    @Test
+    public void testHomeAddBuddies() throws Exception {
+        String url = "http://localhost:" + port;
+
+        HttpEntity<AddressBook> requestAb = new HttpEntity<>(new AddressBook());
+        restTemplate.postForObject(url + "/addressBooks", requestAb, Void.class);
+
+        String name = "Bob";
+        String phoneNumber = "555-333-1111";
+        HttpEntity<BuddyInfo> requestBud = new HttpEntity<>(new BuddyInfo(name, phoneNumber));
+        restTemplate.postForObject(url + "/buddyInfoes", requestBud, Void.class);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(RestMediaTypes.TEXT_URI_LIST);
+        headers.setAccessControlRequestMethod(HttpMethod.PATCH); //need to fake PATCH as a POST for some reason
+        HttpEntity<String> requestPatch = new HttpEntity<>("http://localhost:8080/buddyInfoes/1", headers);
+        restTemplatePatch.postForObject(url + "/addressBooks/1/buddies", requestPatch, Void.class);
+
+        String name2 = "Mob";
+        String phoneNumber2 = "555-333-4444";
+        requestBud = new HttpEntity<>(new BuddyInfo(name2, phoneNumber2));
+        restTemplate.postForObject(url + "/buddyInfoes", requestBud, Void.class);
+        requestPatch = new HttpEntity<>("http://localhost:8080/buddyInfoes/2", headers);
+        restTemplatePatch.postForObject(url + "/addressBooks/1/buddies", requestPatch, Void.class);
+
+        assertThat(this.restTemplate.getForObject(url + "/home?id=1", String.class)).contains(name + "</td>");
+        assertThat(this.restTemplate.getForObject(url + "/home?id=1", String.class)).contains(phoneNumber + "</td>");
+        assertThat(this.restTemplate.getForObject(url + "/home?id=1", String.class)).contains(name2 + "</td>");
+        assertThat(this.restTemplate.getForObject(url + "/home?id=1", String.class)).contains(phoneNumber2 + "</td>");
+    }
+
+    @Test
+    public void testHomeDeleteBuddy() throws Exception {
+        String url = "http://localhost:" + port;
+
+        HttpEntity<AddressBook> requestAb = new HttpEntity<>(new AddressBook());
+        restTemplate.postForObject(url + "/addressBooks", requestAb, Void.class);
+
+        String name = "Bob";
+        String phoneNumber = "555-333-1111";
+        HttpEntity<BuddyInfo> requestBud = new HttpEntity<>(new BuddyInfo(name, phoneNumber));
+        restTemplate.postForObject(url + "/buddyInfoes", requestBud, Void.class);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(RestMediaTypes.TEXT_URI_LIST);
+        headers.setAccessControlRequestMethod(HttpMethod.PATCH); //need to fake PATCH as a POST for some reason
+        HttpEntity<String> requestPatch = new HttpEntity<>("http://localhost:8080/buddyInfoes/1", headers);
+        restTemplatePatch.postForObject(url + "/addressBooks/1/buddies", requestPatch, Void.class);
+
+        String name2 = "Mob";
+        String phoneNumber2 = "555-333-4444";
+        requestBud = new HttpEntity<>(new BuddyInfo(name2, phoneNumber2));
+        restTemplate.postForObject(url + "/buddyInfoes", requestBud, Void.class);
+        requestPatch = new HttpEntity<>("http://localhost:8080/buddyInfoes/2", headers);
+        restTemplatePatch.postForObject(url + "/addressBooks/1/buddies", requestPatch, Void.class);
+
+        restTemplate.delete(url + "/addressBooks/1/buddies/1");
+
+        assertThat(this.restTemplate.getForObject(url + "/home?id=1", String.class)).doesNotContain(name + "</td>");
+        assertThat(this.restTemplate.getForObject(url + "/home?id=1", String.class)).doesNotContain(phoneNumber + "</td>");
+        assertThat(this.restTemplate.getForObject(url + "/home?id=1", String.class)).contains(name2 + "</td>");
+        assertThat(this.restTemplate.getForObject(url + "/home?id=1", String.class)).contains(phoneNumber2 + "</td>");
     }
 }
